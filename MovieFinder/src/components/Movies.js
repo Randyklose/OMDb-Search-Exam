f218806c20c4f63b346cfc636f9b8950ccad6a37
Randyklose <randyklose@gmail.com>
2017-03-27 15:56:06 -0400

    final commit

--- MovieFinder/src/components/Movies.js
+++ MovieFinder/src/components/Movies.js
@@ -1,71 +1,66 @@
 import React from 'react';
 import Loading from './Loading';
-import { Link } from 'react-router';
-import { Paper } from 'material-ui';
+import {Link} from 'react-router';
+import {Paper} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 var API_HOST = 'http://www.omdbapi.com/';
 
 class Movies extends React.Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
+    constructor() {
+        super();
+        this.state = {}
+    }
 
-  fetchData() {
-  fetch(`${API_HOST}?s=${this.props.params.title}`)
-  .then(response => response.json())
-  .then(data => {
-    this.setState({data: data.Search})
-  })
-}
+    fetchData() {
+        fetch(`${API_HOST}?s=${this.props.params.title}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({data: data.Search})
+        })
+    }
 
-componentDidMount(){
-this.fetchData()
-}
+    componentDidMount() {
+        this.fetchData()
+    }
 
-componentDidUpdate(prevProps, prevState){
-if(prevProps.params.title !== this.props.params.title){
-  this.fetchData()
-  }
-}
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.params.title !== this.props.params.title) {
+            this.fetchData()
+        }
+    }
 
-  render() {
-    const { data } = this.state;
-    const style = {
-      'backgroundColor': '#455A64',
-};
-    if (!data) {
-    return (
-    <div className="loading-container">
-      <Loading />
-    </div>);
-}
-    return (
-      <div>
-        <div className="list">
-          <ul className="movie-info__titles">
-            {data.map((data, i) =>
-              <MuiThemeProvider key={i} muiTheme={getMuiTheme(lightBaseTheme)}>
-                <li key={i} className="titles">
-                  <Link to={`movie/${data.Title}/movieDetails`}>
-                    <Paper
-                      key={i}
-                      zDepth={5}
-                      style={style}
-                      className="title-display" >
-                      <p>{data.Title}</p>
-                    </Paper>
-                  </Link>
-                </li>
-              </MuiThemeProvider>
-            )}
-          </ul>
-        </div>
-        {this.props.children}
-      </div>
-            )
+    render() {
+        const {data} = this.state;
+        const style = {
+            'backgroundColor': '#455A64'
+        };
+        if (!data) {
+            return (
+                <div className="loading-container">
+                    <Loading/>
+                </div>
+            );
+        }
+        return (
+            <div>
+              <div className="list">
+                <ul className="movie-info__titles">
+                  {data.map((data, i) => <MuiThemeProvider key={i} muiTheme={getMuiTheme(lightBaseTheme)}>
+                    <li key={i} className="titles">
+                      <Link to={`movie/${data.Title}/movieDetails`}>
+                        <Paper key={i} zDepth={5} style={style} className="title-display">
+                          <p>{data.Title}</p>
+                        </Paper>
+                      </Link>
+                    </li>
+                  </MuiThemeProvider>)}
+                    </ul>
+                </div>
+                {this.props.children}
+            </div>
+        )
     }
 }
 
--- MovieFinder/src/components/SearchBar.js
+++ MovieFinder/src/components/SearchBar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { browserHistory as history } from 'react-router';
+import {browserHistory as history} from 'react-router';
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this._handleChange = this._handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this._handleChange = this._handleChange.bind(this);
+    }
 
-  _handleChange(e) {
-    e.preventDefault();
-    history.push(`/movie/${this.refs.userInput.value}`);
-}
+    _handleChange(e) {
+        e.preventDefault();
+        history.push(`/movie/${this.refs.userInput.value}`);
+    }
 
-  render() {
-    return (
-        <div className="search-page">
-          <form>
-            <input ref="userInput"
-              className="search-movie__input"
-              placeholder="Enter a movie title"
-              type="text"
-              onChange={this._handleChange}/>
-          </form>
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div className="search-page">
+              <form>
+                <input ref="userInput"
+                  className="search-movie__input"
+                  placeholder="Enter a movie title"
+                  type="text"
+                  onChange={this._handleChange}/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SearchBar;
